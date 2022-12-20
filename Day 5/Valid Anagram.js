41b18a54f31b34bf958dd762c42ec431428a1b40
DharshanMohan <109813711+dharshan-m@users.noreply.github.com>
2022-12-20 18:08:42 +0530

    Create Valid Anagram.js

---
+++ Day 5/Valid Anagram.js
@@ -0,0 +1,19 @@
+var isAnagram = function(s, t) {
+  var lenA = s.length;
+  var lenB = t.length;
+  var map = {};
+
+  if (lenA !== lenB) return false;
+
+  for (var i = 0; i < lenA; i++) {
+    if (!map[s[i]]) map[s[i]] = 0;
+    map[s[i]]++;
+  }
+
+  for (var j = 0; j < lenB; j++) {
+    if (!map[t[j]]) return false;
+    map[t[j]]--;
+  }
+
+  return true;
+};
